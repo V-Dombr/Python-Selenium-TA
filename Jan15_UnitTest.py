adda61d2a6f0d999d619e5c74097917583ac0666
Vlad Dombr <v.dombrovsky@gmail.com>
2016-01-15 15:12:24 +0200

    Playing with Test Cases. Add BBC.com check

--- Jan15_UnitTest.py
+++ Jan15_UnitTest.py
@@ -7,7 +7,7 @@ class PythonOrgSearch(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
 
-    def test_search_in_python_org(self):
+    def test_search1_in_python_org(self):
         driver = self.driver
         driver.get("http://www.python.org")
         self.assertIn("Python", driver.title)
@@ -16,14 +16,14 @@ class PythonOrgSearch(unittest.TestCase):
         elem.send_keys(Keys.RETURN)
         assert "No results found." not in driver.page_source
 
-    def test_search_in_bbc_com(self):
+    def test_search2_in_bbc_com(self):
         driver = self.driver
         driver.get("http://www.bbc.com/")
         self.assertIn("BBC", driver.title)
         elem = driver.find_element_by_name("q")
         elem.send_keys("pycon")
         elem.send_keys(Keys.RETURN)
-        assert "No results found." not in driver.page_source
+        assert "Sorry, there are no results for" not in driver.page_source
 
 
     def tearDown(self):
