3e7019ceecd6126cbc32a24e4c4cbd94d2fd6649
Vlad Dombr <v.dombrovsky@gmail.com>
2016-01-27 17:40:53 +0200

    MySQL playing with (locally)

--- Jan_27_mySQL_PM1.py
+++
@@ -1,6 +0,0 @@
-import MySQLdb
-
-db = MySQLdb.connect(host="94.250.236.149", user ="sscb", passwd = "Tc8FsnKf43nEMnFM", db = "sscb", port = "3306")
-
-cur = db.cursor()
-cur.execute("SELECT * FROM campaign")
